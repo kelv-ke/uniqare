8a173a3631e1d304d039c3519ddbcc98f9f63c53
kelv-ke <kelv.services01@gmail.com>
2020-07-22 12:14:23 +0300

    22072020

---
+++ js/odal.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+    $("#myBtn1").click(function() {
+        $("#myModal1").modal();
+    })
+});
+$(document).ready(function() {
+    $("#myBtn2").click(function() {
+        $("#myModal2").modal();
+    })
+});
+$(document).ready(function() {
+    $("#myBtn3").click(function() {
+        $("#myModal3").modal();
+    })
+});
+$(document).ready(function() {
+    $("#myBtn4").click(function() {
+        $("#myModal4").modal();
+    })
+});
+$(document).ready(function() {
+    $("#myBtn5").click(function() {
+        $("#myModal5").modal();
+    })
+});
+$(document).ready(function() {
+    $("#myBtn6").click(function() {
+        $("#myModal6").modal();
+    })
+});
+$(document).ready(function() {
+    $("#myBtn7").click(function() {
+        $("#myModal7").modal();
+    })
+});
+$(document).ready(function() {
+    $("#myBtn8").click(function() {
+        $("#myModal8").modal();
+    })
+});
+$(document).ready(function() {
+    $("#myBtn9").click(function() {
+        $("#myModal9").modal();
+    })
+});
+$(document).ready(function() {
+    $("#myBtn10").click(function() {
+        $("#myModal10").modal();
+    })
+});
+$(document).ready(function() {
+    $("#myBtn11").click(function() {
+        $("#myModal11").modal();
+    })
+});
